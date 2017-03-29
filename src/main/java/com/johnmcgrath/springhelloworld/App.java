d0643ccfb6148bdbb5dd400798b82050972a1eb3
john <sonance207@gmail.com>
2017-03-28 21:52:48 -0500

    added reasource folder for beans

--- src/main/java/com/johnmcgrath/springhelloworld/App.java
+++ src/main/java/com/johnmcgrath/springhelloworld/App.java
@@ -1,6 +1,8 @@
 package com.johnmcgrath.springhelloworld;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * Hello world! for spring
@@ -10,10 +12,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context;
 
-        Person person = new Person();
+        /** ApplicationContext is a bean container,
+         * It is a class that instantiate beans.
+         *
+         * Beans are java objects that are imported via
+         * spring framework
+         */
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Person person = (Person)context.getBean("person");
         person.speak();
+        /**
+         * Closes Application context incase there is a memory leak
+         */
+        ((ClassPathXmlApplicationContext)context).close();
 
     }
 }
